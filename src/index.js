6e2c2a1d429b363fef0f0a316d89edcf1bb65df6
Gustavo <gustavohecastro@gmail.com>
2020-06-30 11:08:14 -0300

    mysql config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const db = require('./models');
+const authController = require('./controllers/auth')
 const app = express();
+
+app.use('/auth', authController);
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(3001, () => {
-    console.log("Listen 3001");
-});
+db.sequelize.sync().then( () => {
+    app.listen(3001, () => {
+        console.log("Listen 3001");
+    }); 
+}).catch( error => console.log(error.message))
+
