4c8c2bcb5fd9e51546681d3e0f3ba42c06ae3bfb
Gustavo <gustavohecastro@gmail.com>
2020-07-02 11:42:31 -0300

    model account

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+const bcrypt = require("bcrypt");
+
+const {Account} = require("../models");
+
 router.get('/sign-in', (req, res) => {
     return res.json("Sign In");
 })
 
-router.get('/sign-up', (req, res) => {
+router.get('/sign-up', async (req, res) => {
+
+    const email = "gustavo@teste.com";
+
+    const password = "123123";
+
+    const hash = bcrypt.hashSync(password , 8)
+
+    const result = await Account.create({email, password})
+    console.log(result)
+    
+
     return res.json("Sign Up");
 })
 
